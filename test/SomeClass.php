f65b361c6b2b02ff9e19823451a9c75a8401d09e
chesanovskiy_dv <chesanovskiy_dv@groupbwt.com>
2015-12-22 08:49:58 +0200

    update namespaces

---
+++ test/SomeClass.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chesanovskiy_dv
+ * Date: 30.11.2015
+ * Time: 14:31
+ */
+
+namespace Core\test;
+
+
+class SomeClass
+{
+    public function say()
+    {
+        echo 'I\'m '.__CLASS__." in ".__DIR__."\n\r";
+    }
+}
---
+++ test/bigtest/btest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chesanovskiy_dv
+ * Date: 30.11.2015
+ * Time: 14:27
+ */
+
+namespace Foo\Bar\Baz;
+
+
+class btest
+{
+    public function say()
+    {
+        echo 'I\'m '.__CLASS__." in ".__DIR__."\n\r";
+    }
+}
